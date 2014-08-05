a1e54d6c62682e119bfc4a2c460eebedb1ef87e8
skrusche63 <skrusche63@googlemail.com>
2014-08-05 17:16:52 +0200

    EsInsight updated

--- src/main/scala/de/kp/spark/elastic/EsInsight.scala
+++ src/main/scala/de/kp/spark/elastic/EsInsight.scala
@@ -1,4 +1,22 @@
 package de.kp.spark.elastic
+/* Copyright (c) 2014 Dr. Krusche & Partner PartG
+* 
+* This file is part of the Spark-ELASTIC project
+* (https://github.com/skrusche63/spark-elastic).
+* 
+* Spark-ELASTIC is free software: you can redistribute it and/or modify it under the
+* terms of the GNU General Public License as published by the Free Software
+* Foundation, either version 3 of the License, or (at your option) any later
+* version.
+* 
+* Spark-ELASTIC is distributed in the hope that it will be useful, but WITHOUT ANY
+* WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+* A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+* You should have received a copy of the GNU General Public License along with
+* Spark-ELASTIC. 
+* 
+* If not, see <http://www.gnu.org/licenses/>.
+*/
 
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
@@ -32,8 +50,7 @@ object EsInsight {
   /**
    * Mixing SQL and other Spark operations
    */
-    val subjects = sqlc.sql("SELECT doc.subject FROM docs").filter(row => row.getString(0).contains("Re"))
-    
+    val subjects = sqlc.sql("SELECT doc.subject FROM docs").filter(row => row.getString(0).contains("Re"))    
     subjects.foreach(subject => println(subject))
     
   }
